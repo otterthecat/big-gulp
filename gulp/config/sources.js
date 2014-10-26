9ae723e595268a113df35b2965657f7bfc3f79ac
Otter <otc@otterthecat.com>
2014-10-26 10:26:49 -0500

    updated source paths

--- gulp/config/sources.js
+++ gulp/config/sources.js
@@ -1,10 +1,10 @@
 module.exports = {
 
-	js: ['./client/*.js'],
+	js: ['./client/**/*.js'],
 
 	app: ['./client/app.js'],
 
 	less: ['./less/*.less'],
 
-	test: ['./test/specs/*.js']
+	test: ['./test/specs/**/*.js']
 };
